48981ca95fc54a5d4f4c457317629734055e4d8c
bharatheasycloud <bharath@easycloud.in>
2023-08-22 19:12:57 +0530

    folder added

---
+++ imgwatermarkapp/android/app/src/main/kotlin/com/example/imgwatermarkapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.imgwatermarkapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ imgwatermarkapp/lib/homepage.dart
@@ -0,0 +1,296 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:image_watermark/image_watermark.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  HomeScreenState createState() => HomeScreenState();
+}
+
+class HomeScreenState extends State<HomeScreen> {
+  final _picker = ImagePicker();
+  Uint8List? imgBytes;
+  Uint8List? imgBytes2;
+  XFile? _image;
+  Uint8List? watermarkedImgBytes;
+  bool isLoading = false;
+  String watermarkText = "", imgname = "image not selected";
+  List<bool> textOrImage = [true, false];
+
+  pickImage() async {
+    final image = await _picker.pickImage(
+      source: ImageSource.camera,
+    );
+    if (image != null) {
+      _image = image;
+      var t = await image.readAsBytes();
+      imgBytes = Uint8List.fromList(t);
+    }
+    watermarkedImgBytes = await ImageWatermark.addTextWatermark(
+      ///image bytes
+      imgBytes: imgBytes!,
+
+      ///watermark text
+      watermarkText: 'ddgfgfhgfhghfghjhgjdfsfdsfdfdsfdsfdsfdsf',
+      dstX: 100,
+      dstY: 100,
+      color: const Color.fromARGB(255, 238, 16, 16),
+    );
+  }
+
+  pickImage2() async {
+    XFile? image = await _picker.pickImage(
+      source: ImageSource.camera,
+    );
+    if (image != null) {
+      _image = image;
+      imgname = image.name;
+      var t = await image.readAsBytes();
+      imgBytes2 = Uint8List.fromList(t);
+    }
+
+    setState(() {});
+  }
+
+  notnull() {
+    if (watermarkedImgBytes != null) {
+      setState(() {});
+    }
+  }
+
+  @override
+  void initState() {
+    notnull();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('image_watermark'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 400,
+              height: 400,
+              child: watermarkedImgBytes == null
+                  ? const FlutterLogo(
+                      size: 100,
+                    )
+                  : Image.memory(watermarkedImgBytes!, fit: BoxFit.contain),
+            ),
+            TextButton(onPressed: pickImage, child: const Text('camera')),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+ 
+
+// import 'package:flutter/foundation.dart';
+// import 'package:flutter/material.dart';
+// import 'package:image_picker/image_picker.dart';
+// import 'package:image_watermark/image_watermark.dart';
+
+// class HomeScreen extends StatefulWidget {
+//   const HomeScreen({Key? key}) : super(key: key);
+
+//   @override
+//   HomeScreenState createState() => HomeScreenState();
+// }
+
+// class HomeScreenState extends State<HomeScreen> {
+//   final _picker = ImagePicker();
+//   Uint8List? imgBytes;
+//   Uint8List? imgBytes2;
+//   XFile? _image;
+//   Uint8List? watermarkedImgBytes;
+//   bool isLoading = false;
+//   String watermarkText = "", imgname = "image not selected";
+//   List<bool> textOrImage = [true, false];
+
+//   pickImage() async {
+//     final image = await _picker.pickImage(
+//       source: ImageSource.camera,
+//     );
+//     if (image != null) {
+//       _image = image;
+//       var t = await image.readAsBytes();
+//       imgBytes = Uint8List.fromList(t);
+//     }
+//     setState(() {});
+//   }
+
+//   pickImage2() async {
+//     XFile? image = await _picker.pickImage(
+//       source: ImageSource.camera,
+//     );
+//     if (image != null) {
+//       _image = image;
+//       imgname = image.name;
+//       var t = await image.readAsBytes();
+//       imgBytes2 = Uint8List.fromList(t);
+//     }
+//     setState(() {});
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('image_watermark'),
+//       ),
+//       body: SingleChildScrollView(
+//         scrollDirection: Axis.vertical,
+//         child: Center(
+//           child: SizedBox(
+//             width: 600,
+//             child: Column(
+//               children: [
+//                 GestureDetector(
+//                   onTap: pickImage,
+//                   child: Container(
+//                       margin: const EdgeInsets.all(15),
+//                       decoration: BoxDecoration(
+//                           border: Border.all(),
+//                           borderRadius:
+//                               const BorderRadius.all(Radius.circular(5))),
+//                       width: 600,
+//                       height: 250,
+//                       child: _image == null
+//                           ? const Column(
+//                               mainAxisAlignment: MainAxisAlignment.center,
+//                               children: [
+//                                 Icon(Icons.add_a_photo),
+//                                 SizedBox(
+//                                   height: 10,
+//                                 ),
+//                                 Text('Click here to choose image')
+//                               ],
+//                             )
+//                           : Image.memory(imgBytes!,
+//                               width: 600, height: 200, fit: BoxFit.fitHeight)),
+//                 ),
+//                 ToggleButtons(
+//                   fillColor: Colors.blue,
+//                   borderRadius: const BorderRadius.all(Radius.circular(8)),
+//                   borderWidth: 3,
+//                   borderColor: Colors.black26,
+//                   selectedBorderColor: Colors.black54,
+//                   selectedColor: Colors.black,
+//                   onPressed: (index) {
+//                     textOrImage = [false, false];
+//                     setState(() {
+//                       textOrImage[index] = true;
+//                     });
+//                   },
+//                   isSelected: textOrImage,
+//                   children: const [
+//                     Padding(
+//                       padding: EdgeInsets.all(8.0),
+//                       child: Text(
+//                         '  Text  ',
+//                       ),
+//                     ),
+//                     // second toggle button
+//                     Padding(
+//                         padding: EdgeInsets.all(8.0),
+//                         child: Text(
+//                           '  Image  ',
+//                         ))
+//                   ],
+//                 ),
+//                 const SizedBox(
+//                   height: 10,
+//                 ),
+//                 textOrImage[0]
+//                     ? Padding(
+//                         padding: const EdgeInsets.all(15),
+//                         child: SizedBox(
+//                           width: 600,
+//                           child: TextField(
+//                             onChanged: (val) {
+//                               watermarkText = val;
+//                             },
+//                             decoration: const InputDecoration(
+//                               border: OutlineInputBorder(),
+//                               labelText: 'Watermark Text',
+//                               hintText: 'Watermark Text',
+//                             ),
+//                           ),
+//                         ),
+//                       )
+//                     : Row(
+//                         children: [
+//                           ElevatedButton(
+//                               onPressed: pickImage2,
+//                               child: const Text('Select Watermark image')),
+//                           Text(imgname)
+//                         ],
+//                       ),
+//                 const SizedBox(
+//                   height: 10,
+//                 ),
+//                 ElevatedButton(
+//                   onPressed: () async {
+//                     setState(() {
+//                       isLoading = true;
+//                     });
+//                     if (textOrImage[0]) {
+//                       watermarkedImgBytes =
+//                           await ImageWatermark.addTextWatermark(
+//                         ///image bytes
+//                         imgBytes: imgBytes!,
+
+//                         ///watermark text
+//                         watermarkText: watermarkText,
+//                         dstX: 20,
+//                         dstY: 30,
+//                       );
+
+//                       /// default : imageWidth/2
+//                     } else {
+//                       watermarkedImgBytes =
+//                           await ImageWatermark.addImageWatermark(
+//                               //image bytes
+//                               originalImageBytes: imgBytes!,
+//                               waterkmarkImageBytes: imgBytes2!,
+//                               imgHeight: 200,
+//                               imgWidth: 200,
+//                               dstY: 400,
+//                               dstX: 400);
+//                     }
+
+//                     setState(() {
+//                       isLoading = false;
+//                     });
+//                   },
+//                   child: const Text('Add Watermark'),
+//                 ),
+//                 const SizedBox(
+//                   height: 10,
+//                 ),
+//                 isLoading ? const CircularProgressIndicator() : Container(),
+//                 watermarkedImgBytes == null
+//                     ? const SizedBox()
+//                     : Image.memory(watermarkedImgBytes!),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
